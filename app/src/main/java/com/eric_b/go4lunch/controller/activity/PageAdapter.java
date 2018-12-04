fc6aadf638a917cd50c53b6b502bea2c138eeaa7
Eric_B <eric.bertaut@outlook.fr>
2018-12-04 09:56:59 +0400

    add PageAdapter

---
+++ app/src/main/java/com/eric_b/go4lunch/controller/activity/PageAdapter.java
@@ -0,0 +1,45 @@
+package com.eric_b.go4lunch.controller.activity;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.eric_b.go4lunch.controller.fragment.ListViewFragment;
+import com.eric_b.go4lunch.controller.fragment.MapFragment;
+import com.eric_b.go4lunch.controller.fragment.WorkmateFragment;
+
+public class PageAdapter extends FragmentPagerAdapter{
+
+    public PageAdapter(FragmentManager mgr) {
+        super(mgr);
+    }
+
+    @Override
+    public int getCount() {
+        return(3);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0: //Page number 1
+                return new MapFragment();
+            case 1: //Page number 2
+                return new ListViewFragment();
+            case 2: //Page number 3
+                return new WorkmateFragment();
+            default:
+                return null;
+        }
+    }
+
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        //title of tabs
+        String[]  namePage = {"Map View","List View","Workmates"};
+        return namePage[position];
+    }
+
+
+    }
